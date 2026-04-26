397ef45e80d28526538341817f04bfc2f08bf4cc
Anas Abdelraouf <anasabdelraouf128@gmail.com>
2026-04-26 19:24:40 +0300

    Add getDisplayDetails method to IDisplayable interface
    Updated IDisplayable interface to include getDisplayDetails method.

--- src/Main Package/IDisplayable.java
+++ src/Main Package/IDisplayable.java
@@ -1,3 +1,6 @@
+package MainPackage;
+
 public interface IDisplayable {
-    void printDetails();
+    // Any class implementing this MUST provide a way to display itself as a string
+    String getDisplayDetails();
 }
